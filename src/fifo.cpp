b43df79183208f7a1b82f2857eb9b02dbd4bf03c
Jacob Nilsson <jacob@jacobnilsson.com>
2025-04-23 21:30:08 +0200

    Fix fifo

--- src/fifo.cpp
+++ src/fifo.cpp
@@ -1,5 +1,6 @@
 /* See LICENSE file for copyright and license details. */
 
+#include <iostream>
 #include <fifo.hpp>
 #include <fcntl.h>
 #include <sys/stat.h>
@@ -11,16 +12,21 @@
 #include <majorna.hpp>
 #include <draw.hpp>
 #include <match.hpp>
-#include <filesystem>
 
 void execute_fifo_cmd() {
     int fd;
-    char fifot[BUFSIZ];
+    char _fifot[BUFSIZ];
 
     done = 0;
 
     fd = open(fifofile.c_str(), O_RDONLY);
-    int r = read(fd, fifot, sizeof(fifot));
+    int r = read(fd, _fifot, sizeof(_fifot));
+    std::string fifot = _fifot;
+
+    // remove newlines
+    fifot.erase(std::remove_if(fifot.begin(), fifot.end(), [](char c) {
+        return c == '\n';
+    }));
 
     if (!r) {
         close(fd);
@@ -32,27 +38,27 @@ void execute_fifo_cmd() {
     /* These are the different commands that we can run
      * by outputting text to the FIFO.
      */
-    if (!strcmp(fifot, "drawmenu")) {
+    if (fifot == "drawmenu") {
         if (!ctx.is_drawing) {
             drawmenu();
         }
-    } else if (!strcmp(fifot, "match")) {
+    } else if (fifot == "match") {
         match();
-    } else if (!strcmp(fifot, "update")) {
+    } else if (fifot == "update") {
         if (!ctx.is_drawing) {
             match();
             drawmenu();
         }
-    } else if (!strcmp(fifot, "test")) {
+    } else if (fifot == "test") {
         fprintf(stderr, "Test print\n");
-    } else if (!strcmp(fifot, "die")) {
+    } else if (fifot == "die") {
         close(fd);
         die("FIFO told me to die.");
-    } else if (!strcmp(fifot, "loadconfig")) {
+    } else if (fifot == "loadconfig") {
 #if CONFIG
         conf_init();
 #endif
-    } else if (!strcmp(fifot, "output")) {
+    } else if (fifot == "output") {
         if (selecteditem) {
             for (int i = 0; i < sel_size; i++) {
                 if (sel_index[i] != -1 && (!selecteditem || selecteditem->index != sel_index[i])) {
@@ -60,164 +66,163 @@ void execute_fifo_cmd() {
                 }
             }
         }
-    } else if (!strcmp(fifot, "output_index")) {
+    } else if (fifot == "output_index") {
         if (printindex && selecteditem) {
             fprintf(stdout, "%d\n", selecteditem->index);
         }
-    } else if (!strcmp(fifot, "toggleinput")) {
+    } else if (fifot == "toggleinput") {
         Arg arg;
         toggleinput(arg);
-    } else if (!strcmp(fifot, "togglelarrow")) {
+    } else if (fifot == "togglelarrow") {
         Arg arg;
         togglelarrow(arg);
-    } else if (!strcmp(fifot, "togglerarrow")) {
+    } else if (fifot == "togglerarrow") {
         Arg arg;
         togglerarrow(arg);
-    } else if (!strcmp(fifot, "toggleitem")) {
+    } else if (fifot == "toggleitem") {
         Arg arg;
         toggleitem(arg);
-    } else if (!strcmp(fifot, "toggleprompt")) {
+    } else if (fifot == "toggleprompt") {
         Arg arg;
         toggleprompt(arg);
-    } else if (!strcmp(fifot, "togglecaps")) {
+    } else if (fifot == "togglecaps") {
         Arg arg;
         togglecaps(arg);
-    } else if (!strcmp(fifot, "togglepowerline")) {
+    } else if (fifot == "togglepowerline") {
         Arg arg;
         togglepowerline(arg);
-    } else if (!strcmp(fifot, "togglecaret")) {
+    } else if (fifot == "togglecaret") {
         Arg arg;
         togglecaret(arg);
-    } else if (!strcmp(fifot, "togglehighlight")) {
+    } else if (fifot == "togglehighlight") {
         Arg arg;
         togglehighlight(arg);
-    } else if (!strcmp(fifot, "togglematchcount")) {
+    } else if (fifot == "togglematchcount") {
         Arg arg;
         togglematchcount(arg);
-    } else if (!strcmp(fifot, "togglemode")) {
+    } else if (fifot == "togglemode") {
         Arg arg;
         togglemode(arg);
-    } else if (!strcmp(fifot, "toggleregex")) {
+    } else if (fifot == "toggleregex") {
         Arg arg;
         toggleregex(arg);
-    } else if (!strcmp(fifot, "togglefuzzy")) {
+    } else if (fifot == "togglefuzzy") {
         Arg arg;
         togglefuzzy(arg);
-    } else if (!strcmp(fifot, "toggleimg")) {
+    } else if (fifot == "toggleimg") {
         Arg arg;
         toggleimg(arg);
-    } else if (!strcmp(fifot, "toggleimgtype")) {
+    } else if (fifot == "toggleimgtype") {
         Arg arg;
         toggleimgtype(arg);
-    } else if (!strcmp(fifot, "screenshot")) {
+    } else if (fifot == "screenshot") {
         Arg arg;
         screenshot(arg);
-    } else if (!strcmp(fifot, "setprofile")) {
+    } else if (fifot == "setprofile") {
         Arg arg;
         setprofile(arg);
-    } else if (!strcmp(fifot, "setlines+")) {
+    } else if (fifot == "setlines+") {
         Arg arg;
         arg.i = 1;
         setlines(arg);
-    } else if (!strcmp(fifot, "setlines-")) {
+    } else if (fifot == "setlines-") {
         Arg arg;
         arg.i = -1;
         setlines(arg);
-    } else if (!strcmp(fifot, "setcolumns+")) {
+    } else if (fifot == "setcolumns+") {
         Arg arg;
         arg.i = 1;
         setcolumns(arg);
-    } else if (!strcmp(fifot, "setcolumns-")) {
+    } else if (fifot == "setcolumns-") {
         Arg arg;
         arg.i = -1;
         setcolumns(arg);
-    } else if (!strcmp(fifot, "setx+")) {
+    } else if (fifot == "setx+") {
         Arg arg;
         arg.i = 1;
         setx(arg);
-    } else if (!strcmp(fifot, "setx-")) {
+    } else if (fifot == "setx-") {
         Arg arg;
         arg.i = -1;
         setx(arg);
-    } else if (!strcmp(fifot, "sety+")) {
+    } else if (fifot == "sety+") {
         Arg arg;
         arg.i = 1;
         sety(arg);
-    } else if (!strcmp(fifot, "sety-")) {
+    } else if (fifot == "sety-") {
         Arg arg;
         arg.i = -1;
         sety(arg);
-    } else if (!strcmp(fifot, "setw+")) {
+    } else if (fifot == "setw+") {
         Arg arg;
         arg.i = 1;
         setw(arg);
-    } else if (!strcmp(fifot, "setw-")) {
+    } else if (fifot == "setw-") {
         Arg arg;
         arg.i = -1;
         setw(arg);
-    } else if (!strcmp(fifot, "moveup")) {
+    } else if (fifot == "moveup") {
         Arg arg;
         moveup(arg);
-    } else if (!strcmp(fifot, "movedown")) {
+    } else if (fifot == "movedown") {
         Arg arg;
         movedown(arg);
-    } else if (!strcmp(fifot, "moveleft")) {
+    } else if (fifot == "moveleft") {
         Arg arg;
         moveleft(arg);
-    } else if (!strcmp(fifot, "moveright")) {
+    } else if (fifot == "moveright") {
         Arg arg;
         moveright(arg);
-    } else if (!strcmp(fifot, "movestart")) {
+    } else if (fifot == "movestart") {
         Arg arg;
         movestart(arg);
-    } else if (!strcmp(fifot, "moveend")) {
+    } else if (fifot == "moveend") {
         Arg arg;
         moveend(arg);
-    } else if (!strcmp(fifot, "movenext")) {
+    } else if (fifot == "movenext") {
         Arg arg;
         movenext(arg);
-    } else if (!strcmp(fifot, "moveprev")) {
+    } else if (fifot == "moveprev") {
         Arg arg;
         moveprev(arg);
-    } else if (!strcmp(fifot, "viewhist")) {
+    } else if (fifot == "viewhist") {
         Arg arg;
         viewhist(arg);
-    } else if (!strcmp(fifot, "backspace")) {
+    } else if (fifot == "backspace") {
         Arg arg;
         backspace(arg);
-    } else if (!strcmp(fifot, "deleteword")) {
+    } else if (fifot == "deleteword") {
         Arg arg;
         deleteword(arg);
-    } else if (!strcmp(fifot, "moveword+")) {
+    } else if (fifot == "moveword+") {
         Arg arg;
         arg.i = +1;
         moveword(arg);
-    } else if (!strcmp(fifot, "moveword-")) {
+    } else if (fifot == "moveword-") {
         Arg arg;
         arg.i = -1;
         moveword(arg);
-    } else if (!strcmp(fifot, "movecursor+")) {
+    } else if (fifot == "movecursor+") {
         Arg arg;
         arg.i = +1;
         movecursor(arg);
-    } else if (!strcmp(fifot, "movecursor-")) {
+    } else if (fifot == "movecursor-") {
         Arg arg;
         arg.i = -1;
         movecursor(arg);
-    } else if (!strcmp(fifot, "clear")) {
+    } else if (fifot == "clear") {
         Arg arg;
         clear(arg);
-    } else if (!strcmp(fifot, "exit_0")) {
+    } else if (fifot == "exit_0") {
         exit(0);
-    } else if (!strcmp(fifot, "exit_1")) {
+    } else if (fifot == "exit_1") {
         exit(1);
     } else {
-        fprintf(stderr, "majorna: Unknown command: %s\n", fifot);
+        std::cerr << "majorna: Unknown command: " << fifot << "\n";
     }
 
     close(fd);
 
-    std::filesystem::remove(fifofile);
     mkfifo(fifofile.c_str(), 0660);
 
     done = 1;
